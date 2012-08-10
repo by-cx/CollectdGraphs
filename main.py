41b8e69f1709ed2d0de5ba4618d73f7af0ecb339
Adam Strauch <cx@initd.cz>
2012-08-10 17:39:09 +0200

    Fixies

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import os
 from plugins.interface import Interface
+from plugins.cpu import CPU
 
-i = Interface("/var/lib/collectd/rrd/web/", "/var/www/graphs/")
+data_dir = "/var/lib/collectd/rrd/"
+graphs_dir = "/var/www/graphs/"
+
+for machine in os.listdir(data_dir):
+	i = Interface(
+		os.path.join(data_dir, machine),
+		os.path.join(graphs_dir, machine),
+	)
+	cpu = CPU(
+		os.path.join(data_dir, machine),
+		os.path.join(graphs_dir, machine),
+	)
+
--- plugins/plugin.py
+++ plugins/plugin.py
@@ -1,14 +1,49 @@
-class Plugin(object):
-    def __init__(self, data_dir, dst_dir):
-        self.data_dir = data_dir
-        self.dst_dir = dst_dir
-        self.filenames = {}
+import os
+import re
+import rrdtool
 
-    def convert(self, parms, path):
+class Plugin(object):
+    def __init__(self, data_dir, dst_dir, size=(400, 120)):
+        """
+            Basic class for plugin_directory
+
+            data_dir - directory with rrd databases
+            dst_dir - directory for graphs
+            size - (x, y) tuple with size of graphs
+        """
+        self._data_dir = data_dir
+        self._dst_dir = dst_dir
+        self.size = size
+
+    @property
+    def data_dir(self):
+        return os.path.join(self._data_dir, self.plugin_directory)
+
+    @property
+    def dst_dir(self):
+        directory = os.path.join(self._dst_dir, self.plugin_directory)
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+        return directory
+
+    def time_ranges(self):
+        return (
+            ("day", "-1d"),
+            ("week", "-1w"),
+            ("month", "-4w"),
+            ("three-months", "-12w"),
+            ("six-months", "-24w"),
+            ("year", "-1y"),
+        )
+
+
+    def convert(self, parms, path, end):
         """Convert variables in parameters
         """
         def convert_map(parm):
             parm = parm.replace("{file}", path)
+            parm = parm.replace("{x}", "%d" % self.size[0])
+            parm = parm.replace("{y}", "%d" % self.size[1])
             parm = parm.replace("$Canvas", "FFFFFF")
             parm = parm.replace("$FullRed", "FF0000")
             parm = parm.replace("$FullGreen", "00E000")
@@ -23,5 +58,32 @@ class Plugin(object):
             parm = parm.replace("$HalfYellow", "F3DFB7")
             parm = parm.replace("$HalfCyan", "B7DFF7")
             parm = parm.replace("$HalfMagenta", "DFB7F7")
+            parm = parm.replace("{START}", end)
             return parm
         return map(convert_map, parms)
+
+    def scan_for_files(self):
+        files = []
+        for filename in os.listdir(self.data_dir):
+            if re.search("\.rrd$", filename):
+                files.append((
+                    filename[:-4].split("-"), # splited filename
+                    "%s.rrd" % filename[:-4], # source rrd
+                    filename[:-4] + "-%s.png", # dst png
+                ))
+        return files        
+
+    def gen_graph(self, parms, source, dst):
+        rrd_path = os.path.join(self.data_dir, source)
+        graph_path = os.path.join(self.dst_dir, dst)
+
+        parms_common = [
+            '--start','{START}', '--end', '-1',
+            '--width','{x}', '--height', '{y}',
+        ]
+        for name, time_range in self.time_ranges():
+            rrdtool.graph(
+                graph_path % name,
+                self.convert(parms_common + parms, rrd_path, time_range)
+            )
+
