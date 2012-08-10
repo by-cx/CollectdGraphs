d2964ba2090206c8ad2e7b72d3b6c4d4532985b4
Adam Strauch <cx@initd.cz>
2012-08-10 14:59:46 +0200

    CPU merge into one directory

--- plugins/cpu.py
+++ plugins/cpu.py
@@ -7,18 +7,18 @@ class CPU(MetaPlugin):
         for filename in os.listdir(self._data_dir):
             if re.match("cpu-[0-9]{1,3}", filename):
                 self.plugin_directory = filename
-                self.name = filename
-                self.graph_meta(self.name + "-%s.png")
+                self.name = "cpu"
+                self.graph_meta(filename + "-%s.png")
 
     def graph_meta(self, *args):
         values = (
-            #('idle', 'ffffff', 'cpu-idle.rrd'),
-            ('nice', '00e000', 'cpu-nice.rrd'),
-            ('user', '0000ff', 'cpu-user.rrd'),
-            ('wait', 'ffb000', 'cpu-wait.rrd'),
-            ('system', 'ff0000', 'cpu-system.rrd'),
-            ('softirq', 'ff00ff', 'cpu-softirq.rrd'),
-            ('interrupt', 'a000a0', 'cpu-interrupt.rrd'),
-            ('steal', '000000', 'cpu-steal.rrd'),
+            #('idle', 'ffffff', '%s/cpu-idle.rrd' % self.plugin_directory),
+            ('nice', '00e000', '%s/cpu-nice.rrd' % self.plugin_directory),
+            ('user', '0000ff', '%s/cpu-user.rrd' % self.plugin_directory),
+            ('wait', 'ffb000', '%s/cpu-wait.rrd' % self.plugin_directory),
+            ('system', 'ff0000', '%s/cpu-system.rrd' % self.plugin_directory),
+            ('softirq', 'ff00ff', '%s/cpu-softirq.rrd' % self.plugin_directory),
+            ('interrupt', 'a000a0', '%s/cpu-interrupt.rrd' % self.plugin_directory),
+            ('steal', '000000', '%s/cpu-steal.rrd' % self.plugin_directory),
         )
         return super(CPU, self).graph_meta(values, *args)
