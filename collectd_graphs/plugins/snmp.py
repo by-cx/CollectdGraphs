dcc4dae4f1097d6bbaed0db956c954610f6d3b17
Adam Strauch <cx@initd.cz>
2012-08-20 23:19:34 +0200

    if_packets for snmp plugin

--- collectd_graphs/plugins/snmp.py
+++ collectd_graphs/plugins/snmp.py
@@ -8,6 +8,8 @@ class Snmp(Plugin):
         for splited, source, dst in self.scan_for_files():
             if splited[0] == "if_octets":
                 self.graph_if_octets(source, dst)
+            if splited[0] == "if_packets":
+                self.graph_if_octets(source, dst)
         
     def graph_if_octets(self, *args):
         parms = [
@@ -43,3 +45,37 @@ class Snmp(Plugin):
             "LINE1:inc_avg#$FullBlue:Incoming",
         ]
         self.gen_graph(parms, *args)
+
+        def graph_if_packets(self, *args):
+            parms = [
+                '-v', 'Packets/s', '--units=si',
+                'DEF:tx_min={file}:tx:MIN',
+                'DEF:tx_avg={file}:tx:AVERAGE',
+                'DEF:tx_max={file}:tx:MAX',
+                'DEF:rx_min={file}:rx:MIN',
+                'DEF:rx_avg={file}:rx:AVERAGE',
+                'DEF:rx_max={file}:rx:MAX',
+                'CDEF:overlap=tx_avg,rx_avg,GT,rx_avg,tx_avg,IF',
+                'CDEF:mytime=tx_avg,TIME,TIME,IF',
+                'CDEF:sample_len_raw=mytime,PREV(mytime),-',
+                'CDEF:sample_len=sample_len_raw,UN,0,sample_len_raw,IF',
+                'CDEF:tx_avg_sample=tx_avg,UN,0,tx_avg,IF,sample_len,*',
+                'CDEF:tx_avg_sum=PREV,UN,0,PREV,IF,tx_avg_sample,+',
+                'CDEF:rx_avg_sample=rx_avg,UN,0,rx_avg,IF,sample_len,*',
+                'CDEF:rx_avg_sum=PREV,UN,0,PREV,IF,rx_avg_sample,+',
+                "AREA:tx_avg#$HalfGreen",
+                "AREA:rx_avg#$HalfBlue",
+                "AREA:overlap#$HalfBlueGreen",
+                "LINE1:tx_avg#$FullGreen:TX",
+                'GPRINT:tx_avg:AVERAGE:%5.1lf%s Avg,',
+                'GPRINT:tx_max:MAX:%5.1lf%s Max,',
+                'GPRINT:tx_avg:LAST:%5.1lf%s Last',
+                'GPRINT:tx_avg_sum:LAST:(ca. %4.0lf%s Total)\l',
+                "LINE1:rx_avg#$FullBlue:RX",
+                #'GPRINT:rx_min:MIN:%5.1lf %s Min,',
+                'GPRINT:rx_avg:AVERAGE:%5.1lf%s Avg,',
+                'GPRINT:rx_max:MAX:%5.1lf%s Max,',
+                'GPRINT:rx_avg:LAST:%5.1lf%s Last',
+                'GPRINT:rx_avg_sum:LAST:(ca. %4.0lf%s Total)\l'
+            ]
+            self.gen_graph(parms, *args)
